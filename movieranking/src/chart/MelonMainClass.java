07560f16c468d703a44fdfcf873af745d2477e26
user <user@lg>
2021-12-04 16:07:35 +0900

    멜론차트 추가

---
+++ movieranking/src/chart/MelonMainClass.java
@@ -0,0 +1,34 @@
+package chart;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.Scanner;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class MelonMainClass {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println("멜론차트100");
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("검색어: ");
+//		String str = sc.next();
+//		String text = URLEncoder.encode(str, "UTF-8");
+		String URL = "https://www.melon.com/chart/";
+		Document doc = Jsoup.connect(URL).get();
+		Elements elem = doc.select("tr#lst50.lst50");
+				
+				for(Element e: elem.select("div")) {
+					if (e.className().equals("wrap_song_info")) {
+						System.out.println(e.text());
+						//continue;
+					}
+					
+				}
+
+	}
+}	
---
+++ movieranking/src/chart/MovieMainClass.java
@@ -0,0 +1,34 @@
+package chart;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.Scanner;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class MovieMainClass {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println("영화 랭킹 출력");
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("검색어: ");
+//		String str = sc.next();
+//		String text = URLEncoder.encode(str, "UTF-8");
+		String URL = "https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=cur&date=20211203";
+		Document doc = Jsoup.connect(URL).get();
+		Elements elem = doc.select("table.list_ranking");
+				
+				for(Element e: elem.select("td")) {
+					if (e.className().equals("title")) {
+						System.out.println(e.text());
+						//continue;
+					}
+					
+				}
+
+	}
+}	
